a9147400b882bf81cb75e20996593718dbe4da71
Brad Siefert <bradsiefert@users.noreply.github.com>
2024-12-24 17:14:53 -0500

    Removing contact page and updating about page conten

--- pages/about.vue
+++ pages/about.vue
@@ -4,18 +4,23 @@
       <h1>About</h1>
       <h2>Bio</h2>
       <p>
-        Hello, I'm a Chicago based photographer and <a target="_new" href="https://bradsiefert.com">UI/UX Designer</a> experienced in portraits and outdoor photography. I'm experienced portraiture, landscapes, travel, and long exposure photography. I shoot instant film to help keep analog photography alive.
+        Hello 👋, I'm a Chicago-based photographer and <a target="_new" href="https://bradsiefert.com">UI/UX Designer</a>. I'm experienced in portraits, landscapes, travel, and long-exposure photography. I shoot instant film to keep analog photography alive.
       </p>
 
-      <h2>Contributed</h2>
+      <h2>Contributed to</h2>
       <p>
-        Special Olympics, Block Club Chicago, Chicago Tribune, Complex, The Red Bulletin, North Park University, Red Letter Christians, Relevant Magazine, The FADER, Vibe
+        Special Olympics, Block Club Chicago, Chicago Tribune, Complex, The Red Bulletin, North Park University, Red Letter Christians, Relevant Magazine, The FADER, Vibe, Christianity Today
       </p>
 
       <h2>Get In Touch</h2>
       <!--<strong>SAY HELLO:</strong> <a href="/contact">Contact Me</a><br />-->
-      <strong>INSTAGRAM:</strong> <a target="_new" href="https://instagram.com/bradsiefert/">@bradsiefert</a><br />
-      <strong>MASTODON:</strong> <a rel="me" href="https://mas.to/@bradsiefert">@bradsiefert@mas.to</a><br />
+      <ul class="list-unstyled">
+        <li><strong>BLUESKY:</strong> <a rel="me" href="https://bsky.app/profile/bradsiefert.bsky.social">@bradsiefert.bsky.social</a></li>
+        <li><strong>INSTAGRAM:</strong> <a target="_new" href="https://instagram.com/bradsiefert/">@bradsiefert</a></li>
+        <li><strong>THREADS:</strong> <a rel="me" href="https://www.threads.net/@bradsiefert">@bradsiefert</a></li>
+        <!--<li><strong>MASTODON:</strong> <a rel="me" href="https://mas.to/@bradsiefert">@bradsiefert@mas.to</a></li>-->
+      </ul>
+
     </div>
 
     <div class="col-md-5 headshot">
