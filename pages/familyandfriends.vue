3dc12a0b22ea82a98ff4b2dc7f563d71dd6ccc8f
Brad Siefert <bradsiefert@users.noreply.github.com>
2024-12-24 15:21:10 -0500

    Create links page

---
+++ pages/familyandfriends.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="content">
+    <div class="col-md-10">
+      <h1>Family &amp; Friends</h1>
+      <p>
+        This page is a private compilation of links to photo projects I've shot over the years only for family and friends. It is not linked from my main website and is unlisted from search engines and LLMs to make it difficult to be found. I will update this page with links to projects throughout the year so check back to see more photos.
+      </p>
+      <p>
+        I post them here because I want a place where folks can see these albums, but I don't want to over post to Facebook or other social media feeds. If you ever see anything you'd like removed from these albums for you own privacy, please let me know.
+      </p>
+
+      <h2>2024</h2>
+      <ul class="list-unstyled" style="margin-bottom: 32px;">
+        <li><a target="_new" href="https://adobe.ly/4gEKiFn">2024-12-15 - Lydia's 6th Birthday Birthday </a></li>
+        <li><a target="_new" href="https://adobe.ly/3ZxKn6A">2024-12-14 - Lydia's 6th Birthday Birthday Party </a></li>
+        <li><a target="_new" href="https://adobe.ly/3YWSMS6">2024-10-31 - Halloween '24</a></li>
+        <li><a target="_new" href="https://adobe.ly/3Dro0so">2024-10-20 - Annual Family Photoshoot (Casey &amp; Mars)</a></li>
+        <li><a target="_new" href="https://adobe.ly/3X3vqIj">2024-08-26 - Lydia's 1st Day of Kindergarten</a></li>
+        <li><a target="_new" href="https://adobe.ly/3zrbB5U">2024-07-01 - July 4th Weekend</a></li>
+        <li><a target="_new" href="https://adobe.ly/3TFR18N">2024-04-22 - Mom's Funeral in Pana</a></li>
+        <li><a target="_new" href="https://adobe.ly/3znKmsU">2024-03-31 - Easter '24</a></li>
+        <li><a target="_new" href="https://adobe.ly/3MsguP8">2024-03-25 - Mike's 36th Skating Birthday Party</a></li>
+      </ul>
+
+      <h2>2023</h2>
+      <ul class="list-unstyled" style="margin-bottom: 32px;">
+        <li><a target="_new" href="https://adobe.ly/3x5iZ5j">2023-12-24 - Christmas in Terre Haute</a></li>
+        <li><a target="_new" href="https://adobe.ly/3IGhWeQ">2023-11-23 - Thanksgiving '23</a></li>
+        <li><a target="_new" href="https://adobe.ly/3tO6gT7">2023-10-27 - Halloween '23</a></li>
+        <li><a target="_new" href="https://adobe.ly/4fmz0Vw">2023-10-14 - Trip to Mexico City</a></li>
+        <li><a target="_new" href="https://adobe.ly/3CbrmyP">2023-09-14 - Birthday Trip to Maine</a></li>
+        <li><a target="_new" href="https://adobe.ly/3MvPMFP">2023-06-23 - Trip to Shawnee National Forest</a></li>
+      </ul>
+
+      <!--<h2>2022 &amp; Before</h2>
+      <ul class="list-unstyled" style="margin-bottom: 32px;">
+        <li>
+          <a target="_new" href="https://adobe.ly/3MsguP8">2013-04-05 - Dr. Tony Campolo photoshoot</a>
+        </li>
+        <li>
+          <a target="_new" href="https://adobe.ly/3MsguP8">2017-02-17 - Earlybird Office</a>
+        </li>
+      </ul>-->
+    </div>
+  </div>
+</template>
