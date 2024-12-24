39239406e47249cf9fa9507a1d6a05d0ef3d20ba
Brad Siefert <bradsiefert@users.noreply.github.com>
2024-12-24 15:43:22 -0500

    Light content updates to links

--- pages/familyandfriends.vue
+++ pages/familyandfriends.vue
@@ -3,7 +3,7 @@
     <div class="col-md-10">
       <h1>Family &amp; Friends</h1>
       <p>
-        This page is a private compilation of links to photo projects I've shot over the years only for family and friends. It is not linked from my main website and is unlisted from search engines and LLMs to make it difficult to be found. I will update this page with links to projects throughout the year so check back to see more photos.
+        This page is a private compilation of links to photo projects I've shot over the years only for family and friends. It is not linked from my main website and is unlisted from search engines and LLMs to make it difficult to find. I will update this page with new links throughout the year so bookmark and check back to see updates.
       </p>
       <p>
         I post them here because I want a place where folks can see these albums, but I don't want to over post to Facebook or other social media feeds. If you ever see anything you'd like removed from these albums for you own privacy, please let me know.
@@ -11,8 +11,8 @@
 
       <h2>2024</h2>
       <ul class="list-unstyled" style="margin-bottom: 32px;">
-        <li><a target="_new" href="https://adobe.ly/4gEKiFn">2024-12-15 - Lydia's 6th Birthday Birthday </a></li>
-        <li><a target="_new" href="https://adobe.ly/3ZxKn6A">2024-12-14 - Lydia's 6th Birthday Birthday Party </a></li>
+        <li>🆕 <a target="_new" href="https://adobe.ly/4gEKiFn">2024-12-15 - Lydia's 6th Birthday Birthday</a></li>
+        <li>🆕 <a target="_new" href="https://adobe.ly/3ZxKn6A">2024-12-14 - Lydia's 6th Birthday Birthday Party</a></li>
         <li><a target="_new" href="https://adobe.ly/3YWSMS6">2024-10-31 - Halloween '24</a></li>
         <li><a target="_new" href="https://adobe.ly/3Dro0so">2024-10-20 - Annual Family Photoshoot (Casey &amp; Mars)</a></li>
         <li><a target="_new" href="https://adobe.ly/3X3vqIj">2024-08-26 - Lydia's 1st Day of Kindergarten</a></li>
@@ -32,15 +32,11 @@
         <li><a target="_new" href="https://adobe.ly/3MvPMFP">2023-06-23 - Trip to Shawnee National Forest</a></li>
       </ul>
 
-      <!--<h2>2022 &amp; Before</h2>
+      <h2>Pre 2023</h2>
       <ul class="list-unstyled" style="margin-bottom: 32px;">
-        <li>
-          <a target="_new" href="https://adobe.ly/3MsguP8">2013-04-05 - Dr. Tony Campolo photoshoot</a>
-        </li>
-        <li>
-          <a target="_new" href="https://adobe.ly/3MsguP8">2017-02-17 - Earlybird Office</a>
-        </li>
-      </ul>-->
+        <li><a target="_new" href="https://adobe.ly/3MHGSV9">2017-02-17 - Earlybird Office</a></li>
+        <li><a target="_new" href="https://adobe.ly/4hPecYK">2013-04-05 - Dr. Tony Campolo photoshoot</a></li>
+      </ul>
     </div>
   </div>
 </template>
