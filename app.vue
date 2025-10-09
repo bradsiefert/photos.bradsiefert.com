3ada8bcacc64e1fd0cd108ff444e42c0c2a974cc
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-10-08 19:15:33 -0500

    Various improvements and updates to make the images larger

--- app.vue
+++ app.vue
@@ -1,34 +1,35 @@
 <template>
-  <div id="floater"></div>
-  <div id="centered">
-    <div class="sidebar-nav">
-      <div class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div class="navbar-brand">
-          <a href="/">
-            <img src="@/images/logo-icon.svg" alt="Brad Siefert Logo"/>Brad Siefert Photography
-          </a>
+  <div id="app-wrapper">
+    <div id="centered">
+      <div class="sidebar-nav">
+        <div class="navbar navbar-default" role="navigation">
+          <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="navbar-brand">
+            <a href="/">
+              <img src="@/images/logo-icon.svg" alt="Brad Siefert Logo"/>Brad Siefert Photography
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div class="navbar-collapse collapse sidebar-navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li><NuxtLink target="_blank" href="https://10years.photos.bradsiefert.com">10 Years</NuxtLink></li>
-          <li><NuxtLink to="/">People</NuxtLink></li>
-          <li><NuxtLink to="/places">Places</NuxtLink></li>
-          <li><NuxtLink to="/instantfilm">Instant Film</NuxtLink></li>
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-        </ul>
-      </div>
-      </div>
-    </div><!-- /sidebar-nav -->
+        <div class="navbar-collapse collapse sidebar-navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><NuxtLink target="_blank" href="https://10years.photos.bradsiefert.com">10 Years</NuxtLink></li>
+            <li><NuxtLink to="/">People</NuxtLink></li>
+            <li><NuxtLink to="/places">Places</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm">Instant Film</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+          </ul>
+        </div>
+        </div>
+      </div><!-- /sidebar-nav -->
 
-    <NuxtPage />
+      <NuxtPage />
 
-  </div><!-- /centered -->
+    </div><!-- /centered -->
+  </div><!-- /app-wrapper -->
 </template>
