a428883fca5a68c2c222845dc03bcf819362efde
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-12-24 13:56:05 -0500

    Removing journal link until it's ready

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,7 @@
               <li><NuxtLink to="/">People</NuxtLink></li>
               <li><NuxtLink to="/places">Places</NuxtLink></li>
               <li><NuxtLink to="/instantfilm">Instant Film</NuxtLink></li>
-              <li><NuxtLink to="/journal">Journal</NuxtLink></li>
+              <!-- <li><NuxtLink to="/journal">Journal</NuxtLink></li> -->
               <li><NuxtLink to="/about">About</NuxtLink></li>
               <li><darkmode /></li>
             </ul>
